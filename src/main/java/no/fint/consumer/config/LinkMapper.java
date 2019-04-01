9555b05290995525b0cc6f9fc3bad0e110911c06
Asgeir Nilsen <asgeir@fintlabs.no>
2019-04-01 11:27:56 +0200

    Buildfix.

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -1,11 +1,10 @@
 package no.fint.consumer.config;
 
-import no.fint.consumer.utils.RestEndpoints;
-import java.util.Map;
 import com.google.common.collect.ImmutableMap;
-
+import no.fint.consumer.utils.RestEndpoints;
 import no.fint.model.administrasjon.arkiv.*;
-import no.fint.model.felles.*;
+
+import java.util.Map;
 
 public class LinkMapper {
 
@@ -13,17 +12,17 @@ public class LinkMapper {
 		return ImmutableMap.<String,String>builder()
 			.put(Dokumentbeskrivelse.class.getName(), contextPath + RestEndpoints.DOKUMENTBESKRIVELSE)
 			.put(Dokumentobjekt.class.getName(), contextPath + RestEndpoints.DOKUMENTOBJEKT)
-			.put(Dokumentstatus.class.getName(), contextPath + RestEndpoints.DOKUMENTSTATUS)
-			.put(Dokumenttype.class.getName(), contextPath + RestEndpoints.DOKUMENTTYPE)
+			.put(DokumentStatus.class.getName(), contextPath + RestEndpoints.DOKUMENTSTATUS)
+			.put(DokumentType.class.getName(), contextPath + RestEndpoints.DOKUMENTTYPE)
 			.put(Journalpost.class.getName(), contextPath + RestEndpoints.JOURNALPOST)
-			.put(Journalposttype.class.getName(), contextPath + RestEndpoints.JOURNALPOSTTYPE)
-			.put(Journalstatus.class.getName(), contextPath + RestEndpoints.JOURNALSTATUS)
+			.put(JournalpostType.class.getName(), contextPath + RestEndpoints.JOURNALPOSTTYPE)
+			.put(JournalStatus.class.getName(), contextPath + RestEndpoints.JOURNALSTATUS)
 			.put(Korrespondansepart.class.getName(), contextPath + RestEndpoints.KORRESPONDANSEPART)
-			.put(Korrespondanseparttype.class.getName(), contextPath + RestEndpoints.KORRESPONDANSEPARTTYPE)
+			.put(KorrespondansepartType.class.getName(), contextPath + RestEndpoints.KORRESPONDANSEPARTTYPE)
 			.put(Sakspart.class.getName(), contextPath + RestEndpoints.SAKSPART)
-			.put(Sakspartrolle.class.getName(), contextPath + RestEndpoints.SAKSPARTROLLE)
+			.put(SakspartRolle.class.getName(), contextPath + RestEndpoints.SAKSPARTROLLE)
 			.put(Saksstatus.class.getName(), contextPath + RestEndpoints.SAKSSTATUS)
-			.put(Tilknyttetregistreringsom.class.getName(), contextPath + RestEndpoints.TILKNYTTETREGISTRERINGSOM)
+			.put(TilknyttetRegistreringSom.class.getName(), contextPath + RestEndpoints.TILKNYTTETREGISTRERINGSOM)
 			.put(Variantformat.class.getName(), contextPath + RestEndpoints.VARIANTFORMAT)
 			/* .put(TODO,TODO) */
 			.build();
