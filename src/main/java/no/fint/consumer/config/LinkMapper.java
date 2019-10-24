ac85f829ddbba14e8856ac3b9f6d93c645699953
Asgeir Nilsen <asgeir@fintlabs.no>
2019-10-24 11:50:17 +0200

    Added Personalressurs to LinkMapper.

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -1,23 +1,20 @@
 package no.fint.consumer.config;
 
+import com.google.common.collect.ImmutableMap;
 import no.fint.consumer.utils.RestEndpoints;
 import no.fint.model.administrasjon.arkiv.*;
 import no.fint.model.administrasjon.personal.Personalressurs;
 
 import java.util.Map;
-import com.google.common.collect.ImmutableMap;
-
-import no.fint.model.administrasjon.arkiv.*;
-import no.fint.model.felles.*;
 
 public class LinkMapper {
 
     public static Map<String, String> linkMapper(String contextPath) {
         return ImmutableMap.<String, String>builder()
-			.put(AdministrativEnhet.class.getName(), contextPath + RestEndpoints.ADMINISTRATIVENHET)
-			.put(Arkivdel.class.getName(), contextPath + RestEndpoints.ARKIVDEL)
-			.put(Arkivressurs.class.getName(), contextPath + RestEndpoints.ARKIVRESSURS)
-			.put(Autorisasjon.class.getName(), contextPath + RestEndpoints.AUTORISASJON)
+                .put(AdministrativEnhet.class.getName(), contextPath + RestEndpoints.ADMINISTRATIVENHET)
+                .put(Arkivdel.class.getName(), contextPath + RestEndpoints.ARKIVDEL)
+                .put(Arkivressurs.class.getName(), contextPath + RestEndpoints.ARKIVRESSURS)
+                .put(Autorisasjon.class.getName(), contextPath + RestEndpoints.AUTORISASJON)
                 .put(Dokumentfil.class.getName(), contextPath + RestEndpoints.DOKUMENTFIL)
                 .put(DokumentStatus.class.getName(), contextPath + RestEndpoints.DOKUMENTSTATUS)
                 .put(DokumentType.class.getName(), contextPath + RestEndpoints.DOKUMENTTYPE)
@@ -28,12 +25,12 @@ public class LinkMapper {
                 .put(Merknadstype.class.getName(), contextPath + RestEndpoints.MERKNADSTYPE)
                 .put(Part.class.getName(), contextPath + RestEndpoints.PART)
                 .put(PartRolle.class.getName(), contextPath + RestEndpoints.PARTROLLE)
-			.put(Rolle.class.getName(), contextPath + RestEndpoints.ROLLE)
+                .put(Rolle.class.getName(), contextPath + RestEndpoints.ROLLE)
                 .put(Sak.class.getName(), contextPath + RestEndpoints.SAK)
                 .put(Saksstatus.class.getName(), contextPath + RestEndpoints.SAKSSTATUS)
                 .put(TilknyttetRegistreringSom.class.getName(), contextPath + RestEndpoints.TILKNYTTETREGISTRERINGSOM)
                 .put(Skjermingshjemmel.class.getName(), contextPath + RestEndpoints.SKJERMINGSHJEMMEL)
-			.put(Tilgang.class.getName(), contextPath + RestEndpoints.TILGANG)
+                .put(Tilgang.class.getName(), contextPath + RestEndpoints.TILGANG)
                 .put(Tilgangsrestriksjon.class.getName(), contextPath + RestEndpoints.TILGANGSRESTRIKSJON)
                 .put(Variantformat.class.getName(), contextPath + RestEndpoints.VARIANTFORMAT)
                 /* .put(TODO,TODO) */
